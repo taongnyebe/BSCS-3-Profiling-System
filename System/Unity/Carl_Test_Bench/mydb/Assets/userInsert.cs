f8360bc4b20ed512fb24596234b696b910baf0d8
JerjerKing69 <jackholeen66@gmail.com>
2021-11-29 16:22:40 +0800

    Cleaning

---
+++ System/Unity/Carl_Test_Bench/mydb/Assets/userInsert.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userInsert : MonoBehaviour
+{
+    string URL = "http://localhost/BSCS-3-Profiling-System/System/PHP/Carl_Test_Bench/mydb/userInsert.php";
+    public string InputUsername, InputEmail, InputPassword;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            AddUser (InputUsername, InputEmail, InputPassword);
+        }
+    }
+
+    public void AddUser(string username, string email, string password){
+        WWWForm form = new WWWForm ();
+        form.AddField ("addUsername", username);
+        form.AddField ("addEmail",  email);
+        form.AddField ("addPassword", password);
+
+        WWW www = new WWW (URL, form);
+    }
+}
---
+++ System/Unity/Carl_Test_Bench/mydb/Assets/userUpdate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userUpdate : MonoBehaviour
+{
+    string URL = "http://localhost/BSCS-3-Profiling-System/System/PHP/Carl_Test_Bench/mydb/userUpdate.php";
+    public string InputUsername, InputEmail, InputPassword, WhereField, WhereCondition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            UpdateUser (InputUsername, InputEmail, InputPassword, WhereField, WhereCondition);
+        }
+    }
+
+    public void UpdateUser(string username, string email, string password, string wF, string wC ){
+        WWWForm form = new WWWForm ();
+        form.AddField("editUsername", username);
+        form.AddField("editEmail", email);
+        form.AddField("editPassword", password);
+
+        form.AddField ("whereField", wF);
+        form.AddField ("whereCondition", wC);
+
+        WWW www = new WWW (URL, form);
+    }
+}
